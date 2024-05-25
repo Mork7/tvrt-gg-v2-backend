8a2b4f866f0c636086c8001201d5d563f65f3f34
mork <mfinerty1@hotmail.com>
2024-05-25 13:54:02 -0400

    Remove friends array, won't be needed

--- models/userModel.js
+++ models/userModel.js
@@ -5,10 +5,6 @@ const userSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     isAdmin: { type: Boolean, required: true, default: false },
-    // An array of friends, where each friend is a reference to the User model
-    friends: [
-        { type: mongoose.Schema.Types.ObjectId, ref: 'User', default: [] },
-    ],
     // An array of accounts one follows, so we can display them in a taable
     following: { type: Array, default: [] },
     summonerDetails: {
